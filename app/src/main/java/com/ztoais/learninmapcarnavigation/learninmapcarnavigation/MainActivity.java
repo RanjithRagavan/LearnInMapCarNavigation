2920c775956931840d98af74cc8d02b9795ce958
ranjithragavan <mailtoranjith07@gmail.com>
2018-10-04 14:57:49 -0700

    intial project setup checkin

---
+++ app/src/main/java/com/ztoais/learninmapcarnavigation/learninmapcarnavigation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ztoais.learninmapcarnavigation.learninmapcarnavigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
